0a3f4f7934f94e57a3dcf779f68d020b5bd165ba
Mas Proxy <afifalfauzan65@gmail.com>
2020-06-10 22:16:04 +0700

    Create DAC.c

---
+++ DAC.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    int RT;
+    char[50] KepalaKeluarga;
+    float penghasilan;//satuan penghasilan (juta)
+}data;
+
+
+void DAC(){
+
+}
+
+int main(){
+    rw[10] data;
+    rw[0].RT = 5;
+    rw[0].penghasilan = 2.5;
+    strcpy(rw[0].KepalaKeluarga,"Budiansyah");
+    
+    rw[1].RT = 4;
+    rw[1].penghasilan = 3.5;
+    strcpy(rw[1].KepalaKeluarga,"Rudi");
+    
+    rw[2].RT = 5;
+    rw[2].penghasilan = 7.5;
+    strcpy(rw[2].KepalaKeluarga,"Imas");
+    
+    rw[3].RT = 4;
+    rw[3].penghasilan = 4;
+    strcpy(rw[3].KepalaKeluarga,"Yuki");
+    
+    rw[3].RT = 4;
+    rw[3].penghasilan = 4;
+    strcpy(rw[3].KepalaKeluarga,"Yupi");
+    
+    rw[4].RT = 1;
+    rw[4].penghasilan = 4.7;
+    strcpy(rw[4].KepalaKeluarga,"Romi");
+    
+    rw[5].RT = 1;
+    rw[5].penghasilan = 2.7;
+    strcpy(rw[5].KepalaKeluarga,"Rossi");
+    
+    rw[5].RT = 2;
+    rw[5].penghasilan = 7;
+    strcpy(rw[5].KepalaKeluarga,"Rusa");
+    
+    rw[6].RT = 3;
+    rw[6].penghasilan = 5;
+    strcpy(rw[6].KepalaKeluarga,"Ryuki");
+    
+    rw[7].RT = 3;
+    rw[7].penghasilan = 5.5;
+    strcpy(rw[7].KepalaKeluarga,"Tomo");
+    
+    rw[8].RT = 2;
+    rw[8].penghasilan = 4;
+    strcpy(rw[8].KepalaKeluarga,"Rudi");
+    
+    rw[9].RT = 3;
+    rw[9].penghasilan = 4.3;
+    strcpy(rw[9].KepalaKeluarga,"Tomi");
+    
+    return 0;
+}
